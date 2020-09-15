cab73640d79ffc6d329b480684faf5ffbc4116ad
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-09-15 08:09:29 -0300

    Create ltpc1-aula04.py

---
+++ LTP1_2020-2/Pratica04/ltpc1-aula04.py
@@ -0,0 +1,10 @@
+#Pede ao usuário um valor para idade
+idade = int(input("Informe sua idade:"))
+
+#Pergunta no código se a idade é igual ou maior a 18
+if idade >= 18:
+  #Este código pertence ao if
+  print("Você possui mais de 18.")
+  print("Agora voocê pode ver todos os jogos da Steam!")
+
+print("Final do programa!")
